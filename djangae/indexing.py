7b186fbe6952c5ae0afe5eb8258516c35760f96d
benvand <benjaminfvandersteen@gmail.com>
2014-11-05 10:13:35 +0000

    Update indexing.py
    It looks like duplicates are extraneous in this index. This change will allow storage of longer values and vastly reduce storage/ iteration on lookup if the value to be indexed contains any duplication. 
    Works for ContainsIndexer and IContainsIndexer index classes.

--- djangae/indexing.py
+++ djangae/indexing.py
@@ -213,7 +213,7 @@ class ContainsIndexer(Indexer):
         result = []
         if value:
             length = len(value)
-            result = [value[i:j + 1] for i in xrange(length) for j in xrange(i, length)]
+            result = list(set([value[i:j + 1] for i in xrange(length) for j in xrange(i, length)]))
             if len(result) > 500:
                 raise ValueError("Can't index for contains query, this value has too many permuatations")
 
